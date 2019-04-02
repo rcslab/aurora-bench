8e7492dd447f4fbfa574c9941e5ec50d13bed6e6
Hongbo <hongbo@skylake5.rcs.uwaterloo.ca>
2019-04-02 01:48:41 +0000

    update scripts

--- scripts/ycsb/tmp2.sh
+++
@@ -1,5 +0,0 @@
-bash ycsb_redis.sh redis.config aof aof
-bash ycsb_redis.sh redis.config sls-100 sls 100
-bash ycsb_redis.sh redis.config sls-500 sls 500
-bash ycsb_redis.sh redis.config sls-1000 sls 1000
-bash ycsb_redis.sh redis.config sls-5000 sls 5000
